f7710b83b7dc9e2ff87c816bdcd7d30ac08c007b
Taylor Rauch <Taylor@students-mbp-4.lan>
2018-05-25 10:35:23 -0400

    day5, material ui contract app

---
+++ week1/contractapp/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import ContractForm from "./ContractForm.js";
+import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      company: "",
+      details: "",
+      submitted: []
+    };
+  }
+
+  updateField(field, newValue) {
+    this.setState({
+      [field]: newValue
+    });
+  }
+  handleClick(e) {
+    let form = {
+      n: this.state.name,
+      c: this.state.company,
+      d: this.state.details
+    };
+    let arr = this.state.submitted;
+    arr.push(form);
+    this.setState({ submitted: arr });
+    this.setState({ name: "", company: "", details: "" });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Contract Form
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <ContractForm
+          updateName={newValue => this.updateField("name", newValue)}
+          updateCompany={newValue => this.updateField("company", newValue)}
+          updateDetails={newValue => this.updateField("details", newValue)}
+          subButton={click => this.handleClick(click)}
+          inputName={this.state.name}
+          inputCompany={this.state.company}
+          inputDetails={this.state.details}
+        />
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Submitted Contracts
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {this.state.submitted.map(obj => (
+          <p style={{ textAlign: "left" }}>
+            <strong>Name:</strong> {obj.n} <br />
+            <strong>Company:</strong> {obj.c} <br />
+            <strong>Details:</strong>
+            {obj.d} <br />
+            <br />
+          </p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week1/contractapp/src/ContractForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+export default class ContractForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <form>
+        <TextField
+          id="name"
+          label="Name"
+          fullWidth
+          margin="normal"
+          value={this.props.inputName}
+          onChange={e => this.props.updateName(e.target.value)}
+        />
+        <TextField
+          id="company"
+          label="Company"
+          fullWidth
+          margin="normal"
+          value={this.props.inputCompany}
+          onChange={e => this.props.updateCompany(e.target.value)}
+        />
+        <TextField
+          id="details"
+          label="Contract Details"
+          multiline
+          rows="4"
+          fullWidth
+          margin="normal"
+          value={this.props.inputDetails}
+          onChange={e => this.props.updateDetails(e.target.value)}
+        />
+        <Button
+          variant="raised"
+          color="primary"
+          onClick={e => this.props.subButton(e)}
+        >
+          SUBMIT
+        </Button>
+        <br />
+        <br />
+      </form>
+    );
+  }
+}
